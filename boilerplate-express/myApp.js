d86a37596c6b97128c160d18a83e1bda6d30e1f2
okhascorpio <okhascorpio@gmail.com>
2022-12-13 18:01:31 +0200

    FCC Basic Node and Express 12/12 Completed

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,49 +1,144 @@
 require('dotenv').config();
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
 
 
 // 1 Meet the Node console
 console.log("Hello World");
 
 
+
+
+// 11 Use body-parser to Parse POST Requests, Place before all routs are defined
+app.use
+(
+  bodyParser.urlencoded({extended: false})
+);
+
+
 // 2 Start a Working Express Server
-app.get("/hello", (req, res) => {
+// http://localhost:3000/hello
+app.get
+(
+  "/hello", (req, res) => 
+  {
   res.send("Hello Express");
-});
+  }
+);
 
 
 // 3 Serve an HTML File
+// http://localhost:3000/
 indexPath = __dirname + "/views/index.html";
 // Serve an HTML File
-app.get("/", (req,res) => {
+app.get
+(
+  "/", (req,res) => 
+  {
     res.sendFile(indexPath);
-});
+  }
+);
 
 
 // 4 Serve Static Assets
 stylePath = __dirname + "/public";
-app.use("/public", express.static(stylePath));
+app.use
+(
+  "/public", express.static(stylePath)
+);
 
 
 // 5 Serve JSON on a Specific Route
-// 6 Use the .env File
-app.get("/json",(req,res) => {
-  if (process.env.MESSAGE_STYLE === "uppercase"){
+// 6 Use the .env File // Add MESSAGE_STYLE=uppercase to the .env file
+// http://localhost:3000/json
+app.get
+(
+  "/json",(req,res) => 
+{
+if (process.env.MESSAGE_STYLE === "uppercase")
+  {
     res.json({"message": "HELLO JSON"});
   } 
-  else {
+  else 
+  {
     res.json({"message": "Hello json"});
   }
-});
+}
+);
 
 
-//7 Implement a Root-Level Request Logger Middleware
-app.use((req,res,next) => {
-  // method path - ip
-  console.log(req.method+" "+req.path+" - "+req.ip);
-  next();
-});
+//7 Implement a Root-Level Request Logger Middleware 
+app.use
+(
+  (req,res,next) => 
+    {
+    // method path - ip
+    console.log(req.method+" "+req.path+" - "+req.ip);
+    next();
+    }
+);
+
+
+
+// 8 Chain Middleware to Create a Time Server
+// http://localhost:3000/now
+app.get
+(
+  "/now", (req, res, next) => 
+    {
+      req.time = new Date().toString(); 
+      next();
+    } , 
+        (req, res) => 
+          {
+           res.send({time: req.time});
+          }
+);
+
+
+// 9 Get Route Parameter Input from the Client
+// http://localhost:3000/Hello/echo
+app.get
+(
+  "/:word/echo", (req, res) => 
+    {
+      res.json({echo: req.params.word});  
+    }
+);
+
+
+// 10 Get Query Parameter Input from the Client
+// http://localhost:3000/qname?first=Hello&last=World
+app.get
+(
+  "/qname", (req, res) =>
+    {
+      var {first: fName, last: lName} = req.query;
+      res.json(
+                {name: `${fName} ${lName}`}
+                //{name: fName+" "+lName} //Also this works
+              );
+    }
+  
+);
+
+
+// 11 Use body-parser to Parse POST Requests, 
+// Place before all routs are defined
+
+
+// 12 Get Data from POST Requests
+app.post
+  (
+    "/name", (req,res) =>
+      {
+        var {first: fName, last: lName} = req.body;
+        res.json(
+                {name: `${fName} ${lName}`}             
+              );
+      }
+  );
 
 
  module.exports = app;
